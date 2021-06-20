fded8a94d8cb8bd7888db3dc16f19c58ca970c42
shintoo <seanprapp@gmail.com>
2021-06-20 12:32:12 -0400

    Epic filters + filter labels

--- src/components/FilterSelector.js
+++ src/components/FilterSelector.js
@@ -1,24 +1,32 @@
-import React from "react"
+import React, { useState } from "react"
 
 let filters = [
   {
     color: "#759133",
-    id: 47126
+    id: 47126, name: "Plantae",
   },
   {
     color: "#914A33",
-    id: 1
+    id: 1, name: "Animalia",
   },
   {
     color: "#D4DADD",
-    id: 3
-  }
+    id: 3, name: "Aves",
+  },
+  {
+    color: "#D289DC",
+    id: 47125, name: "Angiospermae",
+  },
+  {
+    color: "#1B1E54",
+    id: 47120, name: "Arthropoda",
+  },
 ]
 
 // Uncomment out commented-out sections for checkbox-like filtering,
 // Currently is radiobuttion-like filtering
 function FilterSelector(props) {
-  const clickHandler = (event, id) => {
+  const filterClickHandler = (event, id) => {
     event.preventDefault()
     props.resetPage()
     console.log("clicked filter ", id, "filters: ", props.filters)
@@ -54,19 +62,16 @@ function FilterSelector(props) {
     if (props.filters.includes(f.id))
       selected = true
 
-
-
     return (
-      <div
-        key={index}
-        className={classes.join(" ")}
-        style={selected ? selectedStyle : unselectedStyle}
-        onClick={(event) => clickHandler(event, f.id)}>
-
-      </div>
+      <div className="filter-container">
+        <div key={index}
+         className={classes.join(" ")}
+         style={selected ? selectedStyle : unselectedStyle}
+         onClick={(event) => filterClickHandler(event, f.id)}/>
+         <div className="filter-tip">{f.name}</div>
+       </div>
     )
   })
-
 
   const styles = {
     transform: props.hide ? "translateX(-50vw)" : "none",
@@ -74,10 +79,9 @@ function FilterSelector(props) {
     visibility: props.hide ? "hidden": ""
   }
 
-
   return (
     <div style={styles} className="filters-container">
-      {filterButtons}
+      {filterButtons} 
     </div>
   )
 }
